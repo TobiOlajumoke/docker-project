a7cb82ae97a5b6261a58d3d58e40382a9c0d381a
TobiOlajumoke <Olajumoketobi01@gmail.com>
2024-10-15 00:03:34 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Dockerized React Frontend</h1>
+        <p>This is a simple React app running inside a Docker container.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
